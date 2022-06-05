747f2cd052f626d0557862e44206ed8c15e64261
D K <s23109@pjwstk.edu.pl>
2022-06-05 21:25:22 +0200

    init file

---
+++ Apbd10Cw/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apbd10Cw.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
