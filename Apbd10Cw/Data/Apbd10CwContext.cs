b3daaf3365e80e0432a22d1f188c74630962589f
D K <s23109@pjwstk.edu.pl>
2022-06-05 22:53:38 +0200

    Add a model

---
+++ Apbd10Cw/Data/Apbd10CwContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Apbd10Cw.Models;
+
+namespace Apbd10Cw.Data
+{
+    public class Apbd10CwContext : DbContext
+    {
+        public Apbd10CwContext (DbContextOptions<Apbd10CwContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Apbd10Cw.Models.Movie> Movie { get; set; }
+    }
+}
